f10ff6341996d1fc55d5d00ef09626c6b36e1527
Jacob Gillespie <mail@jacobwg.com>
2013-02-08 12:43:51 -0600

    Removed all location code

--- app.rb
+++ app.rb
@@ -1,17 +1,9 @@
 require 'yaml'
-Settings = YAML.load_file('config/settings.yml')
-R = Rosumi.new(Settings['username'], Settings['password'])
 
 class App < Sinatra::Base
 
   get '/' do
     File.read(File.join('public', 'index.html'))
-  end
-
-  get '/location.json' do
-  	content_type 'application/json'
-    position = R.updateDevices.last
-    position['location'].to_json
   end
 
 end
