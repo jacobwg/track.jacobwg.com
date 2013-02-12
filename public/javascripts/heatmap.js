b6a45b5b202865b494ea540ffecd93636111c0fa
Jacob Gillespie <mail@jacobwg.com>
2013-02-12 10:01:58 -0600

    Added viewing options

--- public/javascripts/heatmap.js
+++ public/javascripts/heatmap.js
@@ -2,10 +2,32 @@ var Settings = {
   user_name: 'Jacob'
 };
 
-jQuery(function($) {
-  var map;
+var map;
+var positions, heatmap;
 
-  var positions, positionArray, heatmap;
+var update_data = function() {
+  var uri = 'http://api.jacobwg.com/v1/location/history.json?';
+
+  $('option:selected').each(function(idx){
+    if ($(this).attr('data-option')) {
+      uri += $(this).attr('data-option') + '=' + $(this).attr('data-value') + '&';
+    }
+  });
+
+  console.log(uri);
+
+  $.getJSON(uri + 'callback=?', function(data) {
+    data = $.map(data, function(el, id) {
+      return new google.maps.LatLng(el.latitude, el.longitude);
+    });
+
+    positions = new google.maps.MVCArray(data);
+
+    heatmap.setData(positions);
+  });
+};
+
+jQuery(function($) {
 
   map = new google.maps.Map(document.getElementById("map_canvas"), {
     center: new google.maps.LatLng(33.122026, -96.621323),
@@ -13,24 +35,14 @@ jQuery(function($) {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  var fetchLocationHistory = function() {
-    $.getJSON('http://api.jacobwg.com/v1/location/history.json?not_home=true&callback=?', function(data) {
-      data = $.map(data, function(el, id) {
-        return new google.maps.LatLng(el.latitude, el.longitude);
-      });
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    map: map
+  });
 
-      positions = new google.maps.MVCArray(data);
+  $('select').on('change', function() {
+    update_data();
+  })
 
-      heatmap = new google.maps.visualization.HeatmapLayer({
-        data: positions
-      });
-
-      heatmap.setMap(map);
-
-      window.heatmap = heatmap;
-    });
-  };
-
-  fetchLocationHistory();
+  update_data();
 
 });
